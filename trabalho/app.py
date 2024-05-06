2c4c790fb5fd62707d6d1b924b629266ade8e311
Blauzin <matheusmgirardi@gmail.com>
2024-05-05 22:40:04 -0300

    did more shit

--- trabalho/app.py
+++ trabalho/app.py
@@ -1,16 +1,26 @@
-from flask import Flask, render_template 
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    sensor1_valor = 10
-    sensor1_historico = [9, 11, 12, 10, 9]
+    return render_template('base.html', active_page='home')
 
-    sensor2_valor = 20
-    sensor2_historico = [19, 21, 22, 20, 19]
+@app.route('/sensors')
+def sensors():
+    sensor_data = [
+        {'name': 'Sensor 1', 'image': 'sensor1.jpg', 'value': 25},
+        {'name': 'Sensor 2', 'image': 'sensor2.jpg', 'value': 30}
+    ]
+    return render_template('sensors.html', sensors=sensor_data, active_page='sensors')
 
-    return render_template('index.html', sensor1_valor=sensor1_valor, sensor1_historico=sensor1_historico, sensor2_valor=sensor2_valor, sensor2_historico=sensor2_historico)
+@app.route('/actuators')
+def actuators():
+    actuator_data = [
+        {'name': 'Actuator 1', 'image': 'actuator1.jpg', 'state': 'On'},
+        {'name': 'Actuator 2', 'image': 'actuator2.jpg', 'state': 'Off'}
+    ]
+    return render_template('actuators.html', actuators=actuator_data, active_page='actuators')
 
 if __name__ == '__main__':
     app.run(debug=True)
